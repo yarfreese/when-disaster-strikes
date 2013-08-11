f9c290d4278311f87f5eaa7587ab159545eac06f
yarfreese <yarfreese@gmail.com>
2013-08-10 17:55:51 -0700

    Generate the Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wds::Application.config.secret_key_base = 'a8db7f9ed56405430c9bb44c08cd9f0e39fce3509c92a0937d70ce9aec2a96654dcd50279db611c320329485a9720c8705bb18b596466f04f15c0227f012e23f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wds::Application.config.session_store :cookie_store, key: '_wds_session'
