5227cbd15c9b595c23909ffeab6381b87534b13d
yarfreese <yarfreese@gmail.com>
2013-08-13 11:45:54 -0700

    Add validation to ensure names are specified when creating projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,9 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      #nothing, yet
+      flash[:alert] = "Project has not been created."
+
+      render :action => "new"
     end
   end
 
