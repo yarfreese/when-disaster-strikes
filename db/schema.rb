4708effe33e696846ffab5a72d7c90d8b7ae58e1
yarfreese <yarfreese@gmail.com>
2013-08-18 20:15:55 -0700

    Added admin flag to user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130818233709) do
+ActiveRecord::Schema.define(version: 20130819031322) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20130818233709) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
