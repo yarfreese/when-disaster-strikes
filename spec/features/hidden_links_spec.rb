e609f4ffc9d9e3b065ba203f3d1e4d552fdc346f
yarfreese <yarfreese@gmail.com>
2013-09-06 19:36:05 -0700

    Restrict actions in TicketsController based on permissions and hide links

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -4,6 +4,7 @@ feature "hidden links" do
   let(:user) { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:admin_user) }
   let(:project) { FactoryGirl.create(:project) }
+  let(:post) { FactoryGirl.create(:post, project: project, user: user) }
 
   context "anonymous users" do
     scenario "cannot see the New Project link" do
@@ -24,6 +25,7 @@ feature "hidden links" do
 
   context "regular users" do
     before { sign_in_as!(user) }
+
     scenario "cannot see the New Project link" do
       visit '/'
       assert_no_link_for "New Project"
@@ -38,10 +40,59 @@ feature "hidden links" do
       visit project_path(project)
       assert_no_link_for "Delete Project"
     end
+
+    scenario "New post link is shown to a user with permission" do
+      define_permission!(user, "view", project)
+      define_permission!(user, "create posts", project)
+      visit project_path(project)
+      assert_link_for "New Post"
+    end
+
+    scenario "New post link is hidden from a user without permission" do
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      assert_no_link_for "New Post"
+    end
+
+    scenario "Edit post link is shown to a user with permission" do
+      post
+      define_permission!(user, "view", project)
+      define_permission!(user, "edit posts", project)
+      visit project_path(project)
+      click_link post.title
+      assert_link_for "Edit Post"
+    end
+
+    scenario "Edit post link is hidden from a user without permission" do
+      post
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      click_link post.title
+      assert_no_link_for "Edit Post"
+    end
+
+    scenario "Delete post link is shown to a user with permission" do
+      post
+      define_permission!(user, "view", project)
+      define_permission!(user, "delete posts", project)
+      visit project_path(project)
+      click_link post.title
+      assert_link_for "Delete Post"
+    end
+
+    scenario "Delete post link is hidden from a user without permission" do
+      post
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      click_link post.title
+      assert_no_link_for "Delete Post"
+    end
+
   end
 
   context "admin users" do
     before { sign_in_as!(admin) }
+
     scenario "can see the New Project link" do
       visit '/'
       assert_link_for "New Project"
@@ -56,6 +107,27 @@ feature "hidden links" do
       visit project_path(project)
       assert_link_for "Delete Project"
     end
+
+    scenario "New post link is shown to admins" do
+      visit project_path(project)
+      assert_link_for "New Post"
+    end
+
+    scenario "Edit post link is shown to admins" do
+      post
+      visit project_path(project)
+      click_link post.title
+      assert_link_for "Edit Post"
+    end
+
+    scenario "Delete post link is shown to admins" do
+      post
+      visit project_path(project)
+      click_link post.title
+      assert_link_for "Delete Post"
+    end
+
   end
+
 end
 
