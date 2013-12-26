8e1e90d72d2907fd52b6c6488b0c12f341c9325d
yarfreese <yarfreese@gmail.com>
2013-12-26 08:01:38 -0800

    schema rebuilt because of production error

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131225011006) do
 
-  create_table "assets", force: true do |t|
-    t.string   "asset"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "permissions", force: true do |t|
     t.integer  "user_id"
     t.integer  "thing_id"
